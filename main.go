11ac27e051081ce13c828b0884497a07bccf8111
Kavan Gondalia <kavan155gondalia@gmail.com>
2023-12-13 12:12:29 +0530

    update: database connection

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	// "net/http"
-	"github.com/gin-gonic/gin"
+	"cloud/backend/database"
 	"cloud/backend/routes"
+	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func main() {
+	dbPath := "../../database"
+
+	// database connection
+	kvStore, err := database.NewKVStore(dbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer kvStore.Close()
+
 	router := gin.Default()
-	router.GET("/ping", routes.Ping)
+
+	router.GET("/ping", routes.HandlePing())
+	router.POST("/test", routes.HandleSet(kvStore))
+	router.GET("/test", routes.HandleGet(kvStore))
 
 	router.Run()
 }
