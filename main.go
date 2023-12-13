ce3869764dd9165b1f72b45545594c425c3e3994
Kavan Gondalia <kavan155gondalia@gmail.com>
2023-12-13 10:45:59 +0530

    update: ping server on /ping

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	// "net/http"
+	"github.com/gin-gonic/gin"
+	"cloud/backend/routes"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", routes.Ping)
+
+	router.Run("localhost:8080")
+}
